936499f51ebc7126bf490a3065bb5e441235ac5e
Vanya Ponomarev <ponomarev.vanya@gmail.com>
2023-04-25 18:28:05 -0700

    display init.html if node is already in a network

--- src/ui.py
+++ src/ui.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, render_template, request
-from .admin import admin
+from broadcast import broadcast
+import globals
 
 ui = Blueprint("ui", import_name=__name__, url_prefix="/")
 
 @ui.route('/')
 def index():
-    return render_template('index.html')
+    if (globals.view == []):
+        return render_template('index.html')    
+    
+    return render_template('init.html', addr = globals.view)
 
-@ui.route('/create_view', methods=['POST'])
-def create_view():
-    addresses = request.form.getlist('addresses[]')
+# @ui.route('/create_view', methods=['GET'])
+# def create_view():
+#     return render_template('init.html', addr = globals.view)
 
