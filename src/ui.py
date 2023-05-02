a7caaacbc274f178603fd773e9166ad20d5fc3ce
Vanya Ponomarev <ponomarev.vanya@gmail.com>
2023-05-02 11:41:41 -0700

    created a static folder for js and css files. removed js and css from html files. adding blocking to html files

--- src/ui.py
+++ src/ui.py
@@ -7,11 +7,7 @@ ui = Blueprint("ui", import_name=__name__, url_prefix="/")
 @ui.route('/')
 def index():
     if (globals.view == []):
-        return render_template('index.html')    
-    
-    return render_template('init.html', addr = globals.view)
+        return render_template('new_network.html')
+    return render_template('network.html', addr = globals.view)
 
-# @ui.route('/create_view', methods=['GET'])
-# def create_view():
-#     return render_template('init.html', addr = globals.view)
 
