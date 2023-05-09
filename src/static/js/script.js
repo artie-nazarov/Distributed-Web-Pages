e2283acf686e25f2e2c14554405497ed5a623501
Pavitra Sammandam <pavitra@Pavitras-MacBook-Pro.local>
2023-05-08 18:58:37 -0700

    barebones dynamic searchbar

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -80,3 +80,17 @@ function putData(key, val){
 function loadFile(doc){
         doc.value = "Hello World";
 }
+
+function filterList(){
+        const searchInput = document.querySelector('#search-input');
+        const filter = searchInput.value.toLowerCase();
+        const listItems = document.querySelectorAll('.list-group-item');
+        listItems.forEach((item) =>{
+                let text = item.textContent;
+                if(text.toLowerCase().includes(filter.toLowerCase())){
+                     item.style.display = '';
+                   } else{
+                     item.style.display = 'none';
+                   }
+       });
+}
