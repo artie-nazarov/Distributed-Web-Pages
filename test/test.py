d51166d0a632dab2ac6cefdeba94b5949ff91a92
Vanya Ponomarev <ponomarev.vanya@gmail.com>
2023-05-30 11:47:57 -0700

    setup basic testing framework

--- test/test.py
+++ test/test.py
@@ -1,31 +1,20 @@
-import os
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import unittest
 
-DOCKERFILE_PATH = '../'
-out_file = "out.log"
-err_file = "err.log"
 
-def start_cluster():
-    # build kvs image
-    if os.system(f'docker build -t kvs {DOCKERFILE_PATH} > /dev/null') != 0:
-        raise Exception("Docker build failed")
-    
-    # start cluster, redirect logs to files
-    os.system(f'docker compose -f docker-compose-partition-test.yaml up --wait > {out_file} 2> {err_file}')
+class TestWebUI(unittest.TestCase):
 
-def stop_cluster():
-    if os.system('docker compose -f docker-compose-partition-test.yaml down') != 0:
-        raise Exception("Failed to stop and delete containers")
-    os.system(f"rm -r {out_file} {err_file}")
+    def setUp(self):
+        # Specify the path to chromedriver.exe
+        PATH_TO_DRIVER = 'C:\\chromedriver.exe'
+        s=Service(PATH_TO_DRIVER)
+        self.browser = webdriver.Chrome(service=s)
+        self.addCleanup(self.browser.quit)
 
-def run_tests():
-    # call test function in partition-test-run.py
-    os.system(f'python partition-test-run.py')
-
-def main():
-    start_cluster()
-    run_tests()
-    stop_cluster()
+    def testPageTitle(self):
+        self.browser.get('http://localhost:8080')
+        self.assertIn('Welcome to Decentralized Internet', self.browser.title)
 
 if __name__ == '__main__':
-    main()
+    unittest.main(verbosity=2)
