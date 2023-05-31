fcc03a6dd87e539dbdc25ed82daf018aa955b4aa
Vanya Ponomarev <ponomarev.vanya@gmail.com>
2023-05-31 14:26:50 -0700

    fiddling with selenium web browser

--- test/test.py
+++ test/test.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.alert import Alert
+from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
 
@@ -12,9 +14,20 @@ class TestWebUI(unittest.TestCase):
         self.browser = webdriver.Chrome(service=s)
         self.addCleanup(self.browser.quit)
 
-    def testPageTitle(self):
+    def testCreateNetwork(self):
         self.browser.get('http://localhost:8080')
         self.assertIn('Welcome to Decentralized Internet', self.browser.title)
+        create_button = self.browser.find_element('xpath','//button[@type="submit" and text()="Create"]')
+        create_button.click()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            alert.accept()
+        except NoAlertPresentException:
+            self.fail("No alert present after clicking 'create' button.")
+        else:
+            expected_alert_text = 'Successfully created network'
+            self.assertEqual(alert_text, expected_alert_text)
 
 if __name__ == '__main__':
-    unittest.main(verbosity=2)
+    unittest.main(verbosity=1)
